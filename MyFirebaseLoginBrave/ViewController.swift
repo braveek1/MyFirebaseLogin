048129fe3adee4961a84e9b16f63ebd47ddfc471
Brave Lee <braveek1@gmail.com>
2020-02-01 12:27:11 +0900

    Initial Commit

---
+++ MyFirebaseLoginBrave/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirebaseLoginBrave
+//
+//  Created by YONGKI LEE on 2020/02/01.
+//  Copyright © 2020 Brave Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
