abbe7124ccc1fb423dccf5a188d1f1e40e099302
Brave Lee <braveek1@gmail.com>
2020-02-09 23:53:05 +0900

    first commit

---
+++ MyFirebaseLoginBrave/Views/CameraUIViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CameraUIViewController.swift
+//  MyFirebaseLoginBrave
+//
+//  Created by YONGKI LEE on 2020/02/01.
+//  Copyright © 2020 Brave Lee. All rights reserved.
+//
+
+import UIKit
+
+class CameraUIViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
